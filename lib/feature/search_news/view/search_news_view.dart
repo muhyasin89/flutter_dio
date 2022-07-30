cef9cdf3e81108a36beb34938ccb2069451c6a16
muhyasin89 <muhyasin89@gmail.com>
2022-07-30 22:19:50 +0700

    [news]change search news

--- lib/feature/search_news/view/search_news_view.dart
+++ lib/feature/search_news/view/search_news_view.dart
@@ -9,38 +9,64 @@ class SearchNewsView extends StatelessWidget {
     final controller = Get.find<SearchNewsController>();
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Search"),
-        ),
-        drawer: getAppDrawer(),
-        body: Obx(() {
-          return controller.isLoading.isTrue
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Container(
-                  margin: const EdgeInsets.all(10),
-                  child: ListView.separated(
-                      itemBuilder: (context, index) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(
-                                controller.articles[index].urlToImage),
-                            SizedBox(height: 8),
-                            Text(
-                              controller.articles[index].title,
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w600),
-                            ),
-                            SizedBox(height: 4),
-                            Text(controller.articles[index].description),
-                          ],
-                        );
-                      },
-                      separatorBuilder: (context, index) => Divider(),
-                      itemCount: controller.articles.length),
-                );
-        }));
+      appBar: AppBar(
+        title: Text("Search"),
+      ),
+      drawer: getAppDrawer(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: controller.searchTextController,
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+            ),
+          ),
+          Expanded(child: Obx(() {
+            return controller.isLoading.isTrue
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : controller.articles.isEmpty
+                    ? Center(
+                        child: Text("No news found"),
+                      )
+                    : Container(
+                        margin: const EdgeInsets.all(10),
+                        child: ListView.separated(
+                          itemBuilder: (context, index) {
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                controller.articles[index].urlToImage == null
+                                    ? Container()
+                                    : Image.network(
+                                        controller.articles[index].urlToImage),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  controller.articles[index].title,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  height: 4,
+                                ),
+                                Text(
+                                  controller.articles[index].description,
+                                ),
+                              ],
+                            );
+                          },
+                          separatorBuilder: (context, index) => Divider(),
+                          itemCount: controller.articles.length,
+                        ),
+                      );
+          }))
+        ],
+      ),
+    );
   }
 }
